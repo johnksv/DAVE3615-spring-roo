88f26fc0aad47619f4f896dc2de13f61b14883ff
John Kasper Svergja <john.k.svergja@gmail.com>
2018-02-01 22:37:06 +0100

    Adds email

--- src/main/java/com/s305089/bookstore/email/CustomerServiceImpl.java
+++ src/main/java/com/s305089/bookstore/email/CustomerServiceImpl.java
@@ -1,8 +1,18 @@
 package com.s305089.bookstore.email;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@RequestMapping("/email/**")
+@Controller
 public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
@@ -10,6 +20,17 @@ public class CustomerServiceImpl implements CustomerService {
 
     @Autowired
     private transient MailSender mailTemplate;
+
+    @RequestMapping(method = RequestMethod.POST, value = "{id}")
+    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+
+
+    }
+
+    @RequestMapping
+    public String index() {
+        return "email/index";
+    }
 
     public void sendMessage(String mailTo, String message) {
         org.springframework.mail.SimpleMailMessage mailMessage = new org.springframework.mail.SimpleMailMessage(templateMessage);
